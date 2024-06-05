25cb4f66cefd5d9ebc812cffd4dda6fae5a0ddd7
Loganathan <143515274+Lovely-Logu@users.noreply.github.com>
2024-06-06 00:16:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,6 @@
 import logging
 import logging.config
-
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("imdbpy").setLevel(logging.ERROR)
-
+from aiohttp import web
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media, Media2, choose_mediaDB, db as clientDB
@@ -15,14 +9,19 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, SEC
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
-from Script import script 
-from datetime import date, datetime 
+from Script import script
+from datetime import date, datetime
 import pytz
 from sample_info import tempDict
-from aiohttp import web
 from plugins import web_server
 
-PORT = "8080"
+PORT = 8080
+
+# Setup logging
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 class Bot(Client):
 
@@ -44,30 +43,31 @@ class Bot(Client):
         await super().start()
         await Media.ensure_indexes()
         await Media2.ensure_indexes()
-        #choose the right db by checking the free space
+        # Choose the right db by checking the free space
         stats = await clientDB.command('dbStats')
-        #calculating the free db space from bytes to MB
-        free_dbSize = round(512-((stats['dataSize']/(1024*1024))+(stats['indexSize']/(1024*1024))), 2)
-        if SECONDDB_URI and free_dbSize<10: #if the primary db have less than 10MB left, use second DB.
+        # Calculating the free db space from bytes to MB
+        free_dbSize = round(512 - ((stats['dataSize'] / (1024 * 1024)) + (stats['indexSize'] / (1024 * 1024))), 2)
+        if SECONDDB_URI and free_dbSize < 10:  # If the primary db has less than 10MB left, use second DB.
             tempDict["indexDB"] = SECONDDB_URI
-            logging.info(f"Since Primary DB have only {free_dbSize} MB left, Secondary DB will be used to store datas.")
+            logging.info(f"Since Primary DB has only {free_dbSize} MB left, Secondary DB will be used to store data.")
         elif SECONDDB_URI is None:
-            logging.error("Missing second DB URI !\n\nAdd SECONDDB_URI now !\n\nExiting...")
+            logging.error("Missing second DB URI!\n\nAdd SECONDDB_URI now!\n\nExiting...")
             exit()
         else:
-            logging.info(f"Since primary DB have enough space ({free_dbSize}MB) left, It will be used for storing datas.")
+            logging.info(f"Since primary DB has enough space ({free_dbSize}MB) left, it will be used for storing data.")
         await choose_mediaDB()
         me = await self.get_me()
         temp.ME = me.id
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
+
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
-        await web.TCPSite(app. bind_address, PORT).start()
-       
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        await web.TCPSite(app, bind_address, PORT).start()
+
+        logging.info(f"{me.first_name} for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
         logging.info(script.LOGO)
         tz = pytz.timezone('Asia/Kolkata')
@@ -95,10 +95,10 @@ class Bot(Client):
                 Unique identifier (int) or username (str) of the target chat.
                 For your personal cloud (Saved Messages) you can simply use "me" or "self".
                 For a contact that exists in your Telegram address book you can use his phone number (str).
-                
+
             limit (``int``):
                 Identifier of the last message to be returned.
-                
+
             offset (``int``, *optional*):
                 Identifier of the first message to be returned.
                 Defaults to 0.
@@ -114,7 +114,7 @@ class Bot(Client):
             new_diff = min(200, limit - current)
             if new_diff <= 0:
                 return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            messages = await self.get_messages(chat_id, list(range(current, current + new_diff + 1)))
             for message in messages:
                 yield message
                 current += 1
