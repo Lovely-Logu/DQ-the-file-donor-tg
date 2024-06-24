672efd2a5994539e075ef6e94019c2a7981062bd
Loganathan <143515274+Lovely-Logu@users.noreply.github.com>
2024-06-24 10:50:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -40,44 +40,44 @@ SPELL_CHECK = {}
 @Client.on_message(filters.group & filters.private & filters.text & filters.incoming)
 async def give_filter(client, message):
     if message.chat.id != -1002168913486:
-    async def handle_auto_filter():
-        search = message.text
-        temp_files, temp_offset, total_results = await get_search_results(
-            chat_id=message.chat.id,
-            query=search.lower(),
-            offset=0,
-            filter=True
-        )
-        if total_results == 0:
-            return
-        else:
-            await message.reply_text(
-                text=(
-                    f"<b>Hey {message.from_user.mention}, {str(total_results)} results are found in my database for your "
-                    f"query {search}. Kindly use inline search or make a group and add me as admin to get movie files. "
-                    f"This is a support group so you can't get files from here...\n\n"
-                    f"For Movies, Join @Tr_Movies_Group</b>"
-                ),
-                parse_mode=enums.ParseMode.HTML
+        async def handle_auto_filter():
+            search = message.text
+            temp_files, temp_offset, total_results = await get_search_results(
+                chat_id=message.chat.id,
+                query=search.lower(),
+                offset=0,
+                filter=True
             )
+            if total_results == 0:
+                return
+            else:
+                await message.reply_text(
+                    text=(
+                        f"<b>Hey {message.from_user.mention}, {str(total_results)} results are found in my database for your "
+                        f"query {search}. Kindly use inline search or make a group and add me as admin to get movie files. "
+                        f"This is a support group so you can't get files from here...\n\n"
+                        f"For Movies, Join @Tr_Movies_Group</b>"
+                    ),
+                    parse_mode=enums.ParseMode.HTML
+                )
 
-    if message.chat.id != SUPPORT_CHAT_ID:  # -1002168913486
-        glob = await global_filters(client, message)
-        if not glob:
-            manual = await manual_filters(client, message)
-            if not manual:
-                settings = await get_settings(message.chat.id)
-                try:
-                    if settings.get('auto_ffilter'):
-                        await auto_filter(client, message)
-                except KeyError:
-                    grpid = await active_connection(str(message.from_user.id))
-                    await save_group_settings(grpid, 'auto_ffilter', True)
+        if message.chat.id != SUPPORT_CHAT_ID:  # -1002168913486
+            glob = await global_filters(client, message)
+            if not glob:
+                manual = await manual_filters(client, message)
+                if not manual:
                     settings = await get_settings(message.chat.id)
-                    if settings.get('auto_ffilter'):
-                        await auto_filter(client, message)
-    else:
-        await handle_auto_filter()
+                    try:
+                        if settings.get('auto_ffilter'):
+                            await auto_filter(client, message)
+                    except KeyError:
+                        grpid = await active_connection(str(message.from_user.id))
+                        await save_group_settings(grpid, 'auto_ffilter', True)
+                        settings = await get_settings(message.chat.id)
+                        if settings.get('auto_ffilter'):
+                            await auto_filter(client, message)
+        else:
+            await handle_auto_filter()
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
